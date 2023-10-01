ea71352043f8a8e12d037b2093a428e94ce649ce
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-01 18:36:37 +0300

    Create 1.2_Important_raeas.java

---
+++ 1_Framer/1.2_Important_raeas.java
@@ -0,0 +1,49 @@
+Framer Motion is a React library that allows you to create animations in a declarative way. It is a powerful library for animations in React that enables the animations of React components on a page and while the component is entering and also leaving. Framer Motion can do all of the following:
+
+- Spring animations
+- Simple keyframes syntax
+- Gestures (drag/tap/hover)
+- Layout and shared layout animations
+- SVG paths
+- Exit animations
+- Server-side rendering
+- Variants for orchestrating animations across components
+- CSS variables
+
+Some of the utilities, components, tags, or classes used to create animations in Framer Motion include:
+
+- `motion` component: used to animate elements and can be thought of as a standard HTML or SVG element enhanced with animation capabilities.
+- `AnimatePresence`: used to animate components entering and leaving a page.
+- `variants`: used to define a set of animation variants that can be applied to a component.
+- `whileTap`: used to define an animation that occurs when a component is tapped.
+- `whileInView`: used to define an animation that occurs when a component is in view.
+- `transition`: used to define the duration, delay, and easing function of an animation.
+- `keyframes`: used to define a set of keyframes for an animation.
+
+Some of the important areas and concepts about animation in Framer Motion include:
+
+- Defining animations using the `animate` prop.
+- Using the `useSpring` hook to create spring animations.
+- Using the `useEffect` hook to detect when an element collides with another element.
+- Using easing functions to control the speed of an animation.
+- Using variants to define a set of animation variants that can be applied to a component.
+- Using `AnimatePresence` to animate components entering and leaving a page.
+
+With Framer Motion, you can create various types of animations, including:
+
+- Spring animations
+- Keyframe animations
+- Gestures (drag/tap/hover) animations
+- Layout and shared layout animations
+- SVG path animations
+- Exit animations
+
+In the context of a Next.js and Tailwind CSS app, Framer Motion can be used to create animations for various components, such as buttons, menus, and images. It can also be used to create page transitions and animations for elements that appear on scroll.
+
+Citations:
+[1] https://tsh.io/blog/framer-motion/
+[2] https://youtube.com/watch?v=ugCN_gynFYw
+[3] https://javascript.plainenglish.io/creating-an-animated-react-component-with-next-js-framer-motion-and-tailwind-css-d34674a51316
+[4] https://dev.to/jameswallis/animating-next-js-page-transitions-with-framer-motion-1g9j
+[5] https://youtube.com/watch?v=W85d2B1-rVI
+[6] https://www.linkedin.com/pulse/nextjs-styling-animation-justin-ramel
